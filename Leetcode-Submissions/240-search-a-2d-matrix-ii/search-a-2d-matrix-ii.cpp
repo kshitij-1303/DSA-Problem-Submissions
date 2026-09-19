90aef4e3b2eece2f639b0937c577dcbd88235caf
kshitij-1303 <95118727+kshitij-1303@users.noreply.github.com>
2026-09-19 21:10:18 +0530

    Time: 40 ms (92.86%) | Memory: 18.8 MB (38.33%) - LeetSync

--- Leetcode-Submissions/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ Leetcode-Submissions/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(), n = m ? matrix[0].size() : 0, r = 0, c = n - 1;
-        while (r < m && c >= 0) {
-            if (matrix[r][c] == target) {
+
+        int row = 0;
+        int col = matrix[0].size() - 1;
+
+        while (row < matrix.size() && col >= 0) {
+
+            if (matrix[row][col] == target) {
                 return true;
             }
-            // Apply ternary Operator
-            matrix[r][c] > target ? c-- : r++;
+
+            if (matrix[row][col] > target) {
+                col--;
+            }
+            else {
+                row++;
+            }
         }
+
         return false;
     }
 };
