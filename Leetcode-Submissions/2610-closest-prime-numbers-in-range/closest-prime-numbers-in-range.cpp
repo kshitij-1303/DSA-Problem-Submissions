6945308c4478e87501414e726fbf9b3d945f08e1
kshitij-1303 <95118727+kshitij-1303@users.noreply.github.com>
2026-09-21 17:49:14 +0530

    Time: 78 ms (92.94%) | Memory: 10.9 MB (88.10%) - LeetSync

---
+++ Leetcode-Submissions/2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -0,0 +1,88 @@
+class Solution {
+private:
+    void sieve(int limit, vector<int>& primes) {
+
+        vector<bool> isPrime(limit + 1, true);
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for (int i = 2; i * i <= limit; i++) {
+
+            if (isPrime[i]) {
+
+                for (int j = i * i; j <= limit; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i <= limit; i++) {
+
+            if (isPrime[i]) {
+                primes.push_back(i);
+            }
+        }
+    }
+
+public:
+    vector<int> closestPrimes(int left, int right) {
+
+        int limit = floor(sqrt(right));
+
+        vector<int> primes;
+
+        sieve(limit, primes);
+
+        vector<bool> isPrime(right - left + 1, true);
+
+        // 1 is not prime
+        if (left == 1) {
+            isPrime[0] = false;
+        }
+
+        for (int i = 0; i < primes.size(); i++) {
+
+            int p = primes[i];
+
+            int start = ((left + p - 1) / p) * p;
+
+            // Don't mark the prime itself
+            if (start == p) {
+                start += p;
+            }
+
+            for (int j = start; j <= right; j += p) {
+                isPrime[j - left] = false;
+            }
+        }
+
+        int previous = -1;
+        int first = -1;
+        int second = -1;
+        int minDiff = INT_MAX;
+
+        for (int i = 0; i < isPrime.size(); i++) {
+
+            if (isPrime[i]) {
+
+                int current = left + i;
+
+                if (previous != -1) {
+
+                    int diff = current - previous;
+
+                    if (diff < minDiff) {
+                        minDiff = diff;
+                        first = previous;
+                        second = current;
+                    }
+                }
+
+                previous = current;
+            }
+        }
+
+        return {first, second};
+    }
+};
